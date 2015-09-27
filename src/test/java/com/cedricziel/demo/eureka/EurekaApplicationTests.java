e180170618326e7254fcb91442d3915dc35e7613
Cedric Ziel <cedric@cedric-ziel.com>
2015-09-27 18:42:50 +0200

    [TASK] Initial commit

---
+++ src/test/java/com/cedricziel/demo/eureka/EurekaApplicationTests.java
@@ -0,0 +1,19 @@
+package com.cedricziel.demo.eureka;
+
+import com.cedricziel.demo.eureka.EurekaApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = EurekaApplication.class)
+@WebAppConfiguration
+public class EurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
